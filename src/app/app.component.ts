b14662f37d52f3c425ac56c95733037eb895a536
ugfire <ugfirecompany@gmail.com>
2024-09-05 01:37:46 +0300

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BibleversionsModule } from '../component/bibleversions/bibleversions.module';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "../component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BibleversionsModule,FormsModule],
+  imports: [RouterOutlet, BibleversionsModule, FormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
