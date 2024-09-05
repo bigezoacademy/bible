39b38b45e2affec4d9959c0a563f9bc8ad2962c7
ugfire <ugfirecompany@gmail.com>
2024-09-05 14:02:14 +0300

    added select book

--- src/component/bibleversions/bibleversions.component.ts
+++ src/component/bibleversions/bibleversions.component.ts
@@ -9,7 +9,7 @@ import { BibleService } from '../../services/bible.service';
 export class BibleversionsComponent implements OnInit {
   bibleData: any = {};
   bibleChapter: any[] = [];
-  version: string = '';  // Default or initial value
+  version: string = 'en-kjv';  // Default or initial value
   book: string = '';      // Default or initial value
   chapter: string = '';      // Default or initial value
   verse: string = '';        // Default or initial value
@@ -17,6 +17,22 @@ export class BibleversionsComponent implements OnInit {
   versions: string[] = ["en-kjv"];
   showBibleVerseBool: boolean = true;
   showBibleChapterBool: boolean = false;
+
+  // Array of books of the Bible (with no spaces)
+  books: string[] = [
+    'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+    'Joshua', 'Judges', 'Ruth', '1Samuel', '2Samuel', '1Kings', '2Kings',
+    '1Chronicles', '2Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job',
+    'Psalms', 'Proverbs', 'Ecclesiastes', 'SongOfSolomon', 'Isaiah', 'Jeremiah',
+    'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah',
+    'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah',
+    'Malachi', 'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1Corinthians',
+    '2Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians',
+    '1Thessalonians', '2Thessalonians', '1Timothy', '2Timothy', 'Titus',
+    'Philemon', 'Hebrews', 'James', '1Peter', '2Peter', '1John', '2John',
+    '3John', 'Jude', 'Revelation'
+  ];
+  
 
   constructor(private bibleService: BibleService) { }
 
@@ -48,12 +64,44 @@ export class BibleversionsComponent implements OnInit {
     );
   }
 
+  nextVerse(currentverse: string): void {
+    let currentverseInt = parseInt(currentverse, 10); // Convert string to int
+    let nextVerseInt = currentverseInt + 1; // Increment by 1
+    let nextVerseStr = nextVerseInt.toString(); // Convert back to string
+  
+    this.bibleService.getBibleData(this.version.toLowerCase(), this.book.toLowerCase(), this.chapter.toLowerCase(), nextVerseStr.toLowerCase()).subscribe(
+      data => {
+        this.bibleData = data;
+        console.log(this.bibleData);
+      },
+      error => {
+        console.error('Error fetching Bible data', error);
+      }
+    );
+  }
+
+  previousVerse(currentverse: string): void {
+    let currentverseInt = parseInt(currentverse, 10); // Convert string to int
+    let prevVerseInt = currentverseInt - 1; // Decrement by 1
+    let prevVerseStr = prevVerseInt.toString(); // Convert back to string
+  
+    this.bibleService.getBibleData(this.version.toLowerCase(), this.book.toLowerCase(), this.chapter.toLowerCase(), prevVerseStr.toLowerCase()).subscribe(
+      data => {
+        this.bibleData = data;
+        console.log(this.bibleData);
+      },
+      error => {
+        console.error('Error fetching Bible data', error);
+      }
+    );
+  }
+
   showChapter(): void {
     this.show = true;
     this.bibleService.getBibleChapter(this.version.toLowerCase(), this.book.toLowerCase(), this.chapter.toLowerCase()).subscribe(
       response => {
         console.log('API Response:', response);
-        const chapterData = response.results; // Adjust according to the actual structure
+        const chapterData = response.results;
         this.bibleChapter = this.removeDuplicates(chapterData);
         console.log(this.bibleChapter);
       },
@@ -64,7 +112,6 @@ export class BibleversionsComponent implements OnInit {
   }
   
   removeDuplicates(data: any[]): any[] {
-    // Check if data is an array before attempting to remove duplicates
     if (Array.isArray(data)) {
       return data.filter((value, index, self) =>
         index === self.findIndex((t) =>
@@ -72,11 +119,11 @@ export class BibleversionsComponent implements OnInit {
         )
       );
     }
-    return []; // Return empty array if data is not an array
+    return [];
   }
 
   clearText() {
-    this.bibleData = {}; // Clear bibleData
+    this.bibleData = {};
     this.bibleChapter = [];
   }
 }
